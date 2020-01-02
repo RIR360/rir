dfaf3c85b8ae08c4db255a31c722cd7c29f77bd4
RIR360 <50569315+RIR360@users.noreply.github.com>
2020-01-02 21:25:16 +0600

    basic layout

---
+++ script.js
@@ -0,0 +1,3 @@
+function printf(n){console.log(n)}function qs(n){return document.querySelector(n)}function qsa(n){return document.querySelectorAll(n)}
+
+printf("height " + qs("#navbar").offsetHeight);
