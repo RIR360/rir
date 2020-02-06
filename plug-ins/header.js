ef8d2f3c61b6c6e0a793ba223c2b9a1fdb98850f
Rezowan Islam <50569315+RIR360@users.noreply.github.com>
2020-02-07 00:47:02 +0600

    Update header.js

--- plug-ins/header.js
+++ plug-ins/header.js
@@ -2,7 +2,7 @@ document.write(`
     <header>
         <div id="navbar" class="div">
             <div id="avatar">
-                <a href="https://github.com/RIR360" target="_blank">
+                <a href="https://www.facebook.com/RezowanIslamRizvy" target="_blank">
                     <img src="assets/my_profile.png" alt="">
                     &ensp;<span title="Rezowan Islam Rizvy">Rizvy</span>
                 </a>
@@ -11,7 +11,6 @@ document.write(`
                 <a href="index.html"><li>Home</li></a>
                 <a href="files.html"><li>Files</li></a>
                 <a href="projects.html"><li>Projects</li></a>
-                <a href="contact.html"><li>Contact</li></a>
             </ul>
         </div>
     </header>
