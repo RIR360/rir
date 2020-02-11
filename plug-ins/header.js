fe0d0f2317949dfeb4ddf41bca30e350cd64c7b0
Rezowan Islam <50569315+RIR360@users.noreply.github.com>
2020-02-11 20:21:30 +0600

    Update header.js

--- plug-ins/header.js
+++ plug-ins/header.js
@@ -1,17 +1,16 @@
 document.write(`
-<div id="header">		
-  <ul class="name">
-    <li><a href="#">Rizvy</a></li>
-  </ul>
-  <ul class="links">
-    <li><a href="#" class="active-nav">Home</a></li>
-    <li><a href="#">Files</a></li>
-    <li><a href="#">Projects</a></li>
-  </ul>
-<div class="line">
-	<div></div>
-	<div></div>	
-</div>
-
-</div>
+	<div id="header">		
+	  <ul class="name">
+	    <li><a href="#">Rizvy</a></li>
+	  </ul>
+	  <ul class="links">
+	    <li><a href="#" class="active-nav">Home</a></li>
+	    <li><a href="#">Files</a></li>
+	    <li><a href="#">Projects</a></li>
+	  </ul>
+	<div class="line">
+		<div></div>
+		<div></div>	
+	</div>
+	</div>
 `);
